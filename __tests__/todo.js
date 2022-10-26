3a37cd5e98885c27d1c8c13438217eed192dad67
PiyushBagde <piyushbagde002@gmail.com>
2022-10-26 17:37:19 +0530

    starting test

---
+++ __tests__/todo.js
@@ -0,0 +1,30 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable semi */
+/* eslint-disable quotes */
+/* eslint-disable no-undef */
+const todoList = require("../todo");
+const { all, markAsCompleted, add } = todoList();
+
+describe("TodoList Test Suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+  });
+  test("Should add new todo", () => {
+    const todoItemsCount = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    expect(all.length).toBe(todoItemsCount + 1);
+  });
+  test("Should mark todo as completed", () => {
+    expect(all[0].completed).toBe(false);
+    markAsCompleted(0);
+    expect(all[0].completed).toBe(true);
+  });
+});
